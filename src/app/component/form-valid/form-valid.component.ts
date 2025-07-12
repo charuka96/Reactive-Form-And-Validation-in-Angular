8ef966c8f883ee979008632f4c1ba585fc379e35
charuka96 <chaukathisaru96@gmail.com>
2025-07-12 23:37:20 +0530

    imported reactiveformModule

--- src/app/component/form-valid/form-valid.component.ts
+++ src/app/component/form-valid/form-valid.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-valid',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-valid.component.html',
   styleUrl: './form-valid.component.css'
 })
